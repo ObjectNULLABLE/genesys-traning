8d816060b1398f04117e87cb0b4997c9c74e5f03
mgbrvch <mgbrvch>
2021-07-08 17:30:38 +0300

    feature/button/properties

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
-function Button() {
+function Button({
+  onClick, caption, className, buttonType,
+}) {
   return (
-    <button className={styles.button} type="button">
-      button
+    <button
+      onClick={onClick}
+      className={`${styles.button} ${styles[buttonType]}`}
+      type="button"
+    >
+      {caption}
+      {className}
     </button>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  caption: PropTypes.string,
+  className: PropTypes.string,
+  buttonType: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onClick: () => { alert('test button'); },
+  caption: 'default button',
+  className: '',
+  buttonType: '',
+};
 
 export default Button;
--- src/features/App/App.jsx
+++ src/features/App/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Button from '../../components/button/index';
 
+const onClick = () => {
+  console.log('ghj');
+};
+
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Button onClick={onClick} caption="caption" className="" />
     </div>
   );
 }
