75657af50d60c049b4df5511672cc3bf97260012
mgbrvch <mgbrvch>
2021-07-08 14:51:30 +0300

    feature: add button

---
+++ src/components/button/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function Button() {
+  return (
+    <button className={styles.button} type="button">
+      button
+    </button>
+  );
+}
+
+export default Button;
--- src/features/App/App.jsx
+++ src/features/App/App.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
+import Button from '../../components/button/index';
 
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button />
     </div>
   );
 }
