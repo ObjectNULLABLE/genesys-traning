3dcc6eee937722dc710fc886427c561079e68e4a
qushoneo <pavelgrinevitsch2018@gmail.com>
2021-07-09 12:43:02 +0300

    removed-test-menu

--- src/features/App/App.jsx
+++ src/features/App/App.jsx
@@ -1,27 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Select from '../../components/select/index';
-
-const options = [{ name: 'Skills', value: 'skills' }, { name: 'Heroes', value: 'heroes' }, { name: 'Items', value: 'items' }];
 function App() {
-  const [val, setVal] = useState({});
   return (
-    <div className="App">
-      <div style={{ width: '20%' }}>
-        <Select
-          options={options}
-          selectValue={setVal}
-        />
-        <h1>
-          name:
-          {val.name}
-          <br />
-          value:
-          {val.value}
-        </h1>
-      </div>
-
-    </div>
+    <div className="App" />
   );
 }
 
