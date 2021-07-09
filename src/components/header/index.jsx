9980c16c422f176e05dfbe5faddf13d2fd0294b3
Evgen Shmat <evgeniy.shmat@gmail.com>
2021-07-09 11:51:18 +0300

    fix incorrections

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,55 +1,19 @@
 import React from 'react';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
-import Qual from '../qualities';
-import Weapon from '../weapon';
-import Talents from '../talents';
-import Skills from '../skills';
-import Adversaries from '../adversaries';
-import Sources from '../sources';
 
 function Header() {
   return (
-    <Router>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <Link className={styles.navLink} to="/weapon">Weapons</Link>
-          <Link className={styles.navLink} to="/qualities">Qualities</Link>
-          <Link className={styles.navLink} to="/talents">Talents</Link>
-          <Link className={styles.navLink} to="/skills">Skills</Link>
-          <Link className={styles.navLink} to="/adversaries">Adversaries</Link>
-          <Link className={styles.navLink} to="/sources">Sources</Link>
-        </nav>
-      </header>
-
-      <Switch>
-        <Route path="/weapon">
-          <Weapon />
-        </Route>
-        <Route path="/qualities">
-          <Qual />
-        </Route>
-        <Route path="/talents">
-          <Talents />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/adversaries">
-          <Adversaries />
-        </Route>
-        <Route path="/sources">
-          <Sources />
-        </Route>
-      </Switch>
-    </Router>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link className={styles.navLink} to="/weapon">Weapons</Link>
+        <Link className={styles.navLink} to="/qualities">Qualities</Link>
+        <Link className={styles.navLink} to="/talents">Talents</Link>
+        <Link className={styles.navLink} to="/skills">Skills</Link>
+        <Link className={styles.navLink} to="/adversaries">Adversaries</Link>
+        <Link className={styles.navLink} to="/sources">Sources</Link>
+      </nav>
+    </header>
 
   );
 }
--- src/features/App/App.jsx
+++ src/features/App/App.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import Header from '../../components/header/index';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from 'react-router-dom';
+import Header from '../../components/header';
 import './style.scss';
+import Qual from '../../components/qualities';
+import Weapon from '../../components/weapon';
+import Talents from '../../components/talents';
+import Skills from '../../components/skills';
+import Adversaries from '../../components/adversaries';
+import Sources from '../../components/sources';
 
 function App() {
   return (
     <div>
-      <Header />
+
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/weapon">
+            <Weapon />
+          </Route>
+          <Route path="/qualities">
+            <Qual />
+          </Route>
+          <Route path="/talents">
+            <Talents />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/adversaries">
+            <Adversaries />
+          </Route>
+          <Route path="/sources">
+            <Sources />
+          </Route>
+        </Switch>
+      </Router>
 
     </div>
   );
