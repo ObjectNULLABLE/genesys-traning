e3142ecf28d88eed5d3c3c6a0ba752aab7e82571
mgbrvch <mgbrvch>
2021-07-09 15:42:19 +0300

    feat: fix locate components and path of them

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import Input from './Input';
+import Header from './header';
+import Button from './button';
+import Select from './select';
+
+export default {
+  Button, Input, Header, Select,
+};
