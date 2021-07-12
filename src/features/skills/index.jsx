c1c1e852a0aa0cc574a3238fe3a516a5dc8f922a
polufabricatto <dans2704@gmail.com>
2021-07-12 16:53:14 +0300

    added selectSlice, name: String,

--- src/features/skills/index.jsx
+++ src/features/skills/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Skills() {
+function Skills({ skill }) {
   return (
-    <div>hello skills</div>
+    <div>
+      {skill.name}
+      <br />
+      {skill.shortName}
+    </div>
   );
 }
+
+Skills.propTypes = {
+  skill: PropTypes.objectOf({ name: PropTypes.string, shortName: PropTypes.string }),
+};
+
+Skills.defaultProps = {
+  skill: {
+    name: 'def name',
+    shortName: 'd.name',
+  },
+};
+
 export default Skills;
