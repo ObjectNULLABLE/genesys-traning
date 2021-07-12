538e40e1d8c6d9003ea255fe6d58a323e94d5f5b
polufabricatto <dans2704@gmail.com>
2021-07-12 16:57:02 +0300

    added selectSlice, add layout skills

--- src/features/skills/index.jsx
+++ src/features/skills/index.jsx
@@ -5,20 +5,42 @@ function Skills({ skill }) {
   return (
     <div>
       {skill.name}
-      <br />
-      {skill.shortName}
+      {skill.characteristic}
+      {skill.worlds}
+      {skill.type}
+      {skill.description}
+      {skill.useIf}
+      {skill.notUseIf}
+      {skill.sourseID}
+      {skill.lang}
     </div>
   );
 }
 
 Skills.propTypes = {
-  skill: PropTypes.objectOf({ name: PropTypes.string, shortName: PropTypes.string }),
+  skill: PropTypes.objectOf({
+    name: PropTypes.string,
+    characteristic: PropTypes.string,
+    worlds: PropTypes.array,
+    description: PropTypes.string,
+    useIf: PropTypes.string,
+    notUseIf: PropTypes.string,
+    sourseID: PropTypes.objectOf({ type: PropTypes.string }),
+    lang: PropTypes.string,
+  }),
 };
 
 Skills.defaultProps = {
   skill: {
     name: 'def name',
-    shortName: 'd.name',
+    characteristic: '',
+    worlds: [],
+    type: '',
+    description: '',
+    useIf: '',
+    notUseIf: '',
+    sourceID: 1,
+    lang: '',
   },
 };
 
