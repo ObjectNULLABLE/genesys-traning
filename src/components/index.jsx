9b5bdf98951399f0475b051590cac3c87770fee9
mgbrvch <mgbrvch>
2021-07-12 17:09:11 +0300

    add qualities slice

--- src/components/index.jsx
+++
@@ -1,8 +0,0 @@
-import Input from './Input';
-import Header from './header';
-import Button from './button';
-import Select from './select';
-
-export default {
-  Button, Input, Header, Select,
-};
--- src/features/qualities/index.jsx
+++ src/features/qualities/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
 
-function Qual() {
+function Qual({ qualities }) {
   return (
-    <div>hello qual</div>
+    <div className={styles.qualities}>
+      <div className={styles.name}>
+        {qualities.name}
+      </div>
+      <div className={styles.active}>
+        {qualities.active}
+      </div>
+      <div className={styles.description}>
+        {qualities.description}
+      </div>
+      <div className={styles.sourceID}>
+        {qualities.sourceID}
+      </div>
+      <div className={styles.lang}>
+        {qualities.lang}
+      </div>
+    </div>
   );
 }
+
+Qual.propTypes = {
+  qualities: PropTypes.objectOf({
+    name: PropTypes.string,
+    active: PropTypes.bool,
+    description: PropTypes.string,
+    sourceID: PropTypes.objectOf({ type: PropTypes.string }),
+    lang: PropTypes.string,
+  }),
+};
+
+Qual.defaultProps = {
+  qualities: {
+    name: 'name',
+    active: false,
+    description: 'description',
+    sourceID: 1,
+    lang: 'lang',
+  },
+};
+
 export default Qual;
