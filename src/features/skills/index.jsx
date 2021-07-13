05a5ea7bf0659db240c636f917ac9ee6d7d6f900
polufabricatto <dans2704@gmail.com>
2021-07-13 13:40:41 +0300

    fix console bug, add style to skills page

--- src/features/skills/index.jsx
+++ src/features/skills/index.jsx
@@ -1,47 +1,77 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import style from './style.module.scss';
+import Menu from '../../components/menu';
+import Input from '../../components/Input';
+import Button from '../../components/button';
 
-function Skills({ skill }) {
+const skill = [{
+  name: 'name',
+  characteristic: 'characterictic',
+  worlds: [],
+  type: '1',
+  description: 'description',
+  useIf: 'use',
+  notUseIf: 'notUse',
+  sourceID: 1,
+  lang: 'lang',
+}];
+const content = [{ name: 'obj 1' }, { name: 'obj 2' }, { name: 'obj 3' }, { name: 'obj 4' }];
+
+function Skills() {
   return (
-    <div>
-      {skill.name}
-      {skill.characteristic}
-      {skill.worlds}
-      {skill.type}
-      {skill.description}
-      {skill.useIf}
-      {skill.notUseIf}
-      {skill.sourseID}
-      {skill.lang}
+    <div className={style.sourceBody}>
+      <Menu>
+        {{
+          header: (
+            <div>
+              <div className={style.header}>Skills</div>
+              <Input className={style.search} onChange={() => {}} type="text" />
+            </div>),
+          content: (
+            <div>
+              {content.map((el) => (
+                <div className={style.listElement} key={el.name}>
+                  {el.name}
+                </div>
+              ))}
+            </div>),
+          footer: <Button caption="Add new Skill" className={style.addButton} onClick={() => {}} />,
+        }}
+      </Menu>
+      <div className={style.skills}>
+        <div className={style.wrap1}>
+          <div className={style.name}>
+            {skill[0].name}
+          </div>
+          <div className={style.lang}>
+            {skill[0].lang}
+          </div>
+        </div>
+        <div className={style.description}>
+          {skill[0].description}
+        </div>
+        <div className={style.worlds}>
+          {skill[0].worlds}
+        </div>
+        <div className={style.type}>
+          {skill[0].type}
+        </div>
+        <div className={style.characteristic}>
+          {skill[0].characteristic}
+        </div>
+        <div className={style.useIf}>
+          {skill[0].useIf}
+        </div>
+        <div className={style.notUseIf}>
+          {skill[0].notUseIf}
+        </div>
+        <div className={style.sourceID}>
+          {skill[0].sourseID}
+        </div>
+
+      </div>
     </div>
   );
 }
-
-Skills.propTypes = {
-  skill: PropTypes.objectOf({
-    name: PropTypes.string,
-    characteristic: PropTypes.string,
-    worlds: PropTypes.array,
-    description: PropTypes.string,
-    useIf: PropTypes.string,
-    notUseIf: PropTypes.string,
-    sourseID: PropTypes.objectOf({ type: PropTypes.string }),
-    lang: PropTypes.string,
-  }),
-};
-
-Skills.defaultProps = {
-  skill: {
-    name: 'def name',
-    characteristic: '',
-    worlds: [],
-    type: '',
-    description: '',
-    useIf: '',
-    notUseIf: '',
-    sourceID: 1,
-    lang: '',
-  },
-};
 
 export default Skills;
