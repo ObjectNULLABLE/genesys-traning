243b5ac29136c428d273519fb88d8432f5d7682a
qushoneo <pavelgrinevitsch2018@gmail.com>
2021-07-14 13:56:52 +0300

    deleteSource

--- src/features/sources/sourcesSlice.js
+++ src/features/sources/sourcesSlice.js
@@ -9,8 +9,13 @@ const sourcesSlice = createSlice({
     addSourceAction(state, action) {
       state.sources = [...state.sources, action.payload];
     },
+    deleteSourceAction(state, action) {
+      state.data = state.data.filter(
+        (source) => source.name !== action.payload.name,
+      );
+    },
   },
 });
 
 export default sourcesSlice.reducer;
-export const { addSourceAction } = sourcesSlice.actions;
+export const { addSourceAction, deleteSourceAction } = sourcesSlice.actions;
