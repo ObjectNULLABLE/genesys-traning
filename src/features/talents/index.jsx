b52224e9c83b9919fd8c20ce07f5d4b6aa229a86
Evgen Shmat <evgeniy.shmat@gmail.com>
2021-07-15 15:39:24 +0300

    add icon correct to menu

--- src/features/talents/index.jsx
+++ src/features/talents/index.jsx
@@ -21,7 +21,7 @@ const Talents = () => {
           header: (
             <div>
               <div>Talents</div>
-              <Input className={styles.search} value={filterValue} onChange={setFilterValue} type="text" />
+              <Input value={filterValue} onChange={setFilterValue} type="text" />
             </div>),
           content: (
             <div className={styles.elements}>
@@ -41,15 +41,25 @@ const Talents = () => {
                    className={styles.listElement}
                  >
                    {el.name}
-                   <div
-                     role="button"
-                     tabIndex={0}
-                     onKeyDown={() => {}}
-                     className={styles.cross}
-                     style={selected.name === el.name ? { display: 'block' } : { display: 'none' }}
-                     onClick={() => (window.confirm('Confirm delete?') && deleteTalent(el))}
-                   >
-                     X
+                   <div className={styles.cross}>
+                     <div
+                       role="button"
+                       tabIndex={0}
+                       onKeyDown={() => {}}
+                       style={selected.name === el.name ? { display: 'block' } : { display: 'none' }}
+                       onClick={() => (window.confirm('Confirm delete?') && deleteTalent(el))}
+                     >
+                       <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" className="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z" /></svg>
+                     </div>
+                     <div
+                       role="button"
+                       tabIndex={0}
+                       onKeyDown={() => {}}
+                       style={selected.name === el.name ? { display: 'block' } : { display: 'none' }}
+                       onClick={() => (window.confirm('Confirm delete?') && deleteTalent(el))}
+                     >
+                       <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" className="svg-inline--fa fa-trash-alt fa-w-14" role="img" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z" /></svg>
+                     </div>
                    </div>
                  </div>
                ))
