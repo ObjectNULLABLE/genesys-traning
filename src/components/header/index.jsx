b9285419787b1fa592d465abb956c3201a777569
mgbrvch <mgbrvch>
2021-07-16 15:46:39 +0300

    add adversaries page

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -12,8 +12,8 @@ function Header() {
         <Link className={styles.navLink} to="/skills">Skills</Link>
         <Link className={styles.navLink} to="/adversaries">Adversaries</Link>
         <Link className={styles.navLink} to="/sources">Sources</Link>
-        <Link className={`${styles.navLink} ${styles.account}`} to=" ">Sign In</Link>
-        <Link className={`${styles.navLink} ${styles.account}`} to=" ">Sign Up</Link>
+        <Link className={`${styles.navLink} ${styles.account}`} to="/modal account">Sign In</Link>
+        <Link className={`${styles.navLink} ${styles.account}`} to="/modal account">Sign Up</Link>
       </nav>
     </header>
   );
