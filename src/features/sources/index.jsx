4951a89cc64e9994bb9b31cdafc08a3b72abdcbe
mgbrvch <mgbrvch>
2021-07-20 13:20:42 +0300

    fix style of sources

--- src/features/sources/index.jsx
+++ src/features/sources/index.jsx
@@ -13,7 +13,7 @@ const languages = ['eng', 'ru'];
 const Sources = () => {
   const sources = useSelector((state) => state.sources.data);
   const [filterValue, setFilterValue] = useState('');
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState(sources[0]);
   const [showAdd, setShow] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
 
@@ -120,19 +120,14 @@ const Sources = () => {
         }}
       </Menu>
       <div className={style.features}>
-        {selected
-        && (
-        <div>
-          <div className={style.name}>
-            {selected.name}
-            {` (${selected.shortName})`}
-          </div>
-          <div className={style.description}>
-            <span>Description: </span>
-            {selected.description}
-          </div>
+        <div className={style.name}>
+          {selected.name}
+          {` (${selected.shortName})`}
         </div>
-        )}
+        <div className={style.description}>
+          <span>Description: </span>
+          {selected.description}
+        </div>
         {showAdd && (
           <Modal
             showAdd={showAdd}
